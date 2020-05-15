73ddfdbba5bb8f3a13df4959be6555bf7edb10af
som <som@localhost>
2020-05-15 20:18:57 +0900

    #70 웹소켓 채팅서버 생성 -som

---
+++ fortyeight/src/main/java/com/fortyeight/spring/common/websocket/ChattingServer.java
@@ -0,0 +1,7 @@
+package com.fortyeight.spring.common.websocket;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class ChattingServer extends TextWebSocketHandler {
+
+}
