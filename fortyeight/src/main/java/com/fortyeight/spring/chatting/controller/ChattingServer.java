782e344ffa086fd5183eeb0e868de2203f7ec55c
som <som@localhost>
2020-05-18 20:09:44 +0900

    #79 채팅 서버 구현중 -som

--- fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingServer.java
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingServer.java
@@ -1,16 +1,30 @@
 package com.fortyeight.spring.chatting.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ChattingServer extends TextWebSocketHandler {
+	
+	//세션관리용 객체 생성
+	private Map<String,WebSocketSession> clients=new HashMap<String, WebSocketSession>();
+	
+	@Autowired
+	ObjectMapper mapper;
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		// TODO Auto-generated method stub
-		super.afterConnectionEstablished(session);
+		log.debug("사용자 접속");
 	}
 
 	@Override
