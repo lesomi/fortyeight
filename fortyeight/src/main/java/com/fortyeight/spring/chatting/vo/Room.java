330dd5726e93b13e6b5f5713eb70e2f0365a50ae
som <som@localhost>
2020-05-19 19:19:35 +0900

    #81 채팅 룸 vo 추가 -som

---
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/vo/Room.java
@@ -0,0 +1,15 @@
+package com.fortyeight.spring.chatting.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+	private int roomNo;
+	private int mkNo;
+	private int seller;
+	private int buyer;
+}
