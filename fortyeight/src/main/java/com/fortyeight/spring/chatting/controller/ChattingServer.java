03b657182c8175bcb2b62ae14e07ddd9f989b2c6
som <som@localhost>
2020-05-19 20:45:06 +0900

    #84 채팅 서버 구현중 -som

--- fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingServer.java
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingServer.java
@@ -5,11 +5,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fortyeight.spring.chatting.vo.Chatting;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,15 +29,29 @@ public class ChattingServer extends TextWebSocketHandler {
 	}
 
 	@Override
-	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-		// TODO Auto-generated method stub
-		super.handleMessage(session, message);
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		log.debug(message.getPayload());
+		Chatting msg=getMessage(message.getPayload());
+		
+		
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		// TODO Auto-generated method stub
 		super.afterConnectionClosed(session, status);
+	}
+	
+	private Chatting getMessage(String chatting) {
+		Chatting message=null;
+		
+		try {
+			message=mapper.readValue(chatting, Chatting.class);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return message;
 	}
 	
 }
