6aae622c437ca056ebd84a68c04a83689cff4db1
som <som@192.168.35.67>
2020-05-22 20:56:18 +0900

    #98 채팅 서버 구현중 -som

--- fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingServer.java
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingServer.java
@@ -1,7 +1,10 @@
 package com.fortyeight.spring.chatting.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
@@ -18,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ChattingServer extends TextWebSocketHandler {
 	
 	//세션관리용 객체 생성
-	private Map<String,WebSocketSession> clients=new HashMap<String, WebSocketSession>();
+	private Map<Integer,WebSocketSession> clients=new HashMap<Integer, WebSocketSession>();
 	
 	@Autowired
 	ObjectMapper mapper;
@@ -33,13 +36,23 @@ public class ChattingServer extends TextWebSocketHandler {
 		log.debug(message.getPayload());
 		Chatting msg=getMessage(message.getPayload());
 		
-		
+		switch(msg.getChatType()) {
+			case "new":addClient(msg,session);break;
+			case "msg":sendMessage(msg,session);break;
+		}
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		// TODO Auto-generated method stub
-		super.afterConnectionClosed(session, status);
+		log.debug("사용자 접속 끊김");
+		Set<Map.Entry<Integer, WebSocketSession>> entry=clients.entrySet();
+		Iterator<Map.Entry<Integer, WebSocketSession>> it=entry.iterator();
+		while(it.hasNext()) {
+			Map.Entry<Integer, WebSocketSession> client=it.next();
+			if(!client.getValue().isOpen()) { //client의 session이 열려있지않으면
+				it.remove();
+			}
+		}
 	}
 	
 	private Chatting getMessage(String chatting) {
@@ -52,6 +65,27 @@ public class ChattingServer extends TextWebSocketHandler {
 		}
 		
 		return message;
+	}
+	
+	//클라이언트 등록하기
+	private void addClient(Chatting msg,WebSocketSession session) {
+		clients.put(msg.getSender(), session);
+		log.info("접속자 : "+clients);
+		
+		//사용자에게 접속 축하 메세지 발송
+		Chatting serverMsg=new Chatting(0,"new",1,0,null,"채팅 접속 성공",0);
+		sendMessage(serverMsg, session);
+	}
+	
+	private void sendMessage(Chatting msg,WebSocketSession session) {
+		Set<Map.Entry<Integer, WebSocketSession>> entry=clients.entrySet();
+		for(Map.Entry<Integer, WebSocketSession> client : entry) {
+			try {
+				client.getValue().sendMessage(new TextMessage(mapper.writeValueAsString(msg)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
