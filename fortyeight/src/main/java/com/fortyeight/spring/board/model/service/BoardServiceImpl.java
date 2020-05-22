3a6578c01fa877f3514f7ab2e26069d1a5067f12
som <som@192.168.35.67>
2020-05-22 21:38:51 +0900

    #102 BoardService 내용 추가 -som

--- fortyeight/src/main/java/com/fortyeight/spring/board/model/service/BoardServiceImpl.java
+++ fortyeight/src/main/java/com/fortyeight/spring/board/model/service/BoardServiceImpl.java
@@ -1,8 +1,20 @@
 package com.fortyeight.spring.board.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fortyeight.spring.user.model.service.UserServiceImpl;
 
 @Service
 public class BoardServiceImpl implements BoardService {
-
+	
+	@Autowired
+	private BoardDao dao;
+	@Autowired
+	private SqlSessionTemplate session;
+	@Autowired
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 }
