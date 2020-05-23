2cd1f7b72b4c04c288f641a0d23774e4cb7298bc
som <som@192.168.35.67>
2020-05-23 16:14:03 +0900

    #106 boardList controller 구현 -som

--- fortyeight/src/main/java/com/fortyeight/spring/board/controller/boardController.java
+++ fortyeight/src/main/java/com/fortyeight/spring/board/controller/boardController.java
@@ -1,11 +1,18 @@
 package com.fortyeight.spring.board.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fortyeight.spring.board.model.service.BoardService;
+import com.fortyeight.spring.board.model.vo.Board;
+import com.fortyeight.spring.common.PagingFactory;
 import com.fortyeight.spring.user.controller.UserController;
 
 @Controller
@@ -15,4 +22,21 @@ public class boardController {
 	private BoardService service;
 	@Autowired
 	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping("/board/boardList.do")
+	public ModelAndView selectBoardList(ModelAndView mv,
+			@RequestParam(required=false, defaultValue="1") int cPage,
+			@RequestParam(required=false, defaultValue="10") int numPerPage) {
+		List<Board> list=service.selectList(cPage,numPerPage);
+		
+		int totalData = service.selectBoardCount();
+		
+		mv.addObject("board",list);
+		
+		mv.addObject("pageBar",PagingFactory.getPage(totalData, cPage, numPerPage, "/spring/board/boardList.do"));
+		
+		mv.setViewName("board/boardList");
+		
+		return mv;
+	}
 }
