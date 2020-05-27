4cdbc05da7791abad52b7c8915dd61bef533aee5
som <som@localhost>
2020-05-27 19:24:09 +0900

    #139 찜 vo 추가 -som

---
+++ fortyeight/src/main/java/com/fortyeight/spring/market/model/vo/Dips.java
@@ -0,0 +1,13 @@
+package com.fortyeight.spring.market.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dips {
+	private int userNo;
+	private int mkNo;
+}
