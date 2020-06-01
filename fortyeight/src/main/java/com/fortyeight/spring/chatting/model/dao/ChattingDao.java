e20a253e9c11d589229273c21c1f87a8dcbd1b59
som <som@localhost>
2020-06-01 19:03:13 +0900

    #118 채팅 목록 dao 구현 -som

--- fortyeight/src/main/java/com/fortyeight/spring/chatting/model/dao/ChattingDao.java
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/model/dao/ChattingDao.java
@@ -1,9 +1,12 @@
 package com.fortyeight.spring.chatting.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.fortyeight.spring.chatting.model.vo.Chatting;
 
 public interface ChattingDao {
 	int insertChatting(SqlSessionTemplate session,Chatting msg);
+	List<Chatting> selectChatting(SqlSessionTemplate session,int userNo);
 }
--- fortyeight/src/main/java/com/fortyeight/spring/chatting/model/dao/ChattingDaoImpl.java
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/model/dao/ChattingDaoImpl.java
@@ -1,5 +1,7 @@
 package com.fortyeight.spring.chatting.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,11 @@ public class ChattingDaoImpl implements ChattingDao {
 	@Override
 	public int insertChatting(SqlSessionTemplate session, Chatting msg) {
 		return session.insert("chatting.insertChatting",msg);
+	}
+
+	@Override
+	public List<Chatting> selectChatting(SqlSessionTemplate session, int userNo) {
+		return session.selectList("chatting.selectChatting",userNo);
 	}
 
 }
