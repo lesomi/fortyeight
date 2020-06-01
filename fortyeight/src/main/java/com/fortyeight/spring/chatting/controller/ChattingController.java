f3185e46f7448a1224969d2a2295e69b466d6114
som <som@localhost>
2020-06-01 19:02:16 +0900

    #116 채팅목록 controller 구현 -som

--- fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingController.java
+++ fortyeight/src/main/java/com/fortyeight/spring/chatting/controller/ChattingController.java
@@ -1,12 +1,34 @@
 package com.fortyeight.spring.chatting.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fortyeight.spring.chatting.model.service.ChattingService;
+import com.fortyeight.spring.chatting.model.vo.Chatting;
 
 @Controller
 public class ChattingController {
+	
+	@Autowired
+	private ChattingService service;
+	
 	@RequestMapping("/chattingView.do")
 	public String chattingView() {
 		return "chatting/chattingView";
 	}
+	
+	@RequestMapping("/chatting/chattingList.do")
+	public ModelAndView chattingList(int userNo,ModelAndView mv) {
+		List<Chatting> list=service.selectChatting(userNo);
+		
+		mv.addObject("chat",list);
+		mv.setViewName("chatting/chattingList");
+		
+		return mv;
+	}
+	
 }
