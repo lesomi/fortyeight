be9e681c562479047813f622485e7c0683e0facb
som <som@localhost>
2020-06-03 05:00:28 +0900

    #223 게시판 댓글 dao 구현 -som

--- fortyeight/src/main/java/com/fortyeight/spring/board/model/dao/BoardDao.java
+++ fortyeight/src/main/java/com/fortyeight/spring/board/model/dao/BoardDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.fortyeight.spring.board.model.vo.Board;
+import com.fortyeight.spring.board.model.vo.BoardComment;
 
 public interface BoardDao {
 	List<Board> selectList(SqlSessionTemplate session,int cPage,int numPerPage);
@@ -13,4 +14,8 @@ public interface BoardDao {
 	Board selectBoardView(SqlSessionTemplate session, int boardNo);
 	int deleteBoard(SqlSessionTemplate session,int boardNo);
 	int updateBoard(SqlSessionTemplate session,Board b);
+	int insertBoardComm(SqlSessionTemplate session,BoardComment bc);
+	List<BoardComment> selectBoardComm(SqlSessionTemplate session,int boardNo,int cPage,int numPerPage);
+	int selectboardCommCount(SqlSessionTemplate session,int boardNo);
+	int deleteBoardComm(SqlSessionTemplate session, int boardCommNo);
 }
--- fortyeight/src/main/java/com/fortyeight/spring/board/model/dao/BoardDaoImpl.java
+++ fortyeight/src/main/java/com/fortyeight/spring/board/model/dao/BoardDaoImpl.java
@@ -7,6 +7,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.fortyeight.spring.board.model.vo.Board;
+import com.fortyeight.spring.board.model.vo.BoardComment;
 
 @Repository
 public class BoardDaoImpl implements BoardDao {
@@ -39,6 +40,26 @@ public class BoardDaoImpl implements BoardDao {
 	@Override
 	public int updateBoard(SqlSessionTemplate session, Board b) {
 		return session.update("board.updateBoard",b);
+	}
+
+	@Override
+	public int insertBoardComm(SqlSessionTemplate session, BoardComment bc) {
+		return session.insert("board.insertBoardComm",bc);
+	}
+
+	@Override
+	public List<BoardComment> selectBoardComm(SqlSessionTemplate session, int boardNo,int cPage,int numPerPage) {
+		return session.selectList("board.selectBoardComm",boardNo, new RowBounds((cPage-1)*numPerPage, numPerPage));
+	}
+
+	@Override
+	public int selectboardCommCount(SqlSessionTemplate session, int boardNo) {
+		return session.selectOne("board.selectboardCommCount",boardNo);
+	}
+
+	@Override
+	public int deleteBoardComm(SqlSessionTemplate session, int boardCommNo) {
+		return session.delete("board.deleteBoardComm",boardCommNo);
 	}
 
 }
