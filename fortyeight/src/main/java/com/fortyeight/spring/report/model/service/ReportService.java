ef1ea6187c462904ba1f0f688df4184028ed17ba
som <som@localhost>
2020-06-02 19:17:58 +0900

    #205 신고 리스트 service 구현 -som

--- fortyeight/src/main/java/com/fortyeight/spring/report/model/service/ReportService.java
+++ fortyeight/src/main/java/com/fortyeight/spring/report/model/service/ReportService.java
@@ -1,7 +1,11 @@
 package com.fortyeight.spring.report.model.service;
 
+import java.util.List;
+
 import com.fortyeight.spring.report.model.vo.Report;
 
 public interface ReportService {
 	int insertReport(Report r);
+	List<Report> selectReport(int cPage, int numPerPage);
+	int selectReportCount();
 }
--- fortyeight/src/main/java/com/fortyeight/spring/report/model/service/ReportServiceImpl.java
+++ fortyeight/src/main/java/com/fortyeight/spring/report/model/service/ReportServiceImpl.java
@@ -1,5 +1,7 @@
 package com.fortyeight.spring.report.model.service;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,16 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public int insertReport(Report r) {
 		return dao.insertReport(session,r);
+	}
+
+	@Override
+	public List<Report> selectReport(int cPage, int numPerPage) {
+		return dao.selectReport(session,cPage,numPerPage);
+	}
+
+	@Override
+	public int selectReportCount() {
+		return dao.selectReportCount(session);
 	}
 	
 }
