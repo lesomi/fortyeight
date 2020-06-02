1ff130914a0d0f8a0afbdb7531c435fe23c7e2b5
som <som@localhost>
2020-06-02 19:18:21 +0900

    #206 신고 리스트 dao 구현 -som

--- fortyeight/src/main/java/com/fortyeight/spring/report/model/dao/ReportDao.java
+++ fortyeight/src/main/java/com/fortyeight/spring/report/model/dao/ReportDao.java
@@ -1,9 +1,13 @@
 package com.fortyeight.spring.report.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.fortyeight.spring.report.model.vo.Report;
 
 public interface ReportDao {
 	int insertReport(SqlSessionTemplate session,Report r);
+	List<Report> selectReport(SqlSessionTemplate session,int cPage,int numPerPage);
+	int selectReportCount(SqlSessionTemplate session);
 }
--- fortyeight/src/main/java/com/fortyeight/spring/report/model/dao/ReportDaoImpl.java
+++ fortyeight/src/main/java/com/fortyeight/spring/report/model/dao/ReportDaoImpl.java
@@ -1,5 +1,8 @@
 package com.fortyeight.spring.report.model.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +14,16 @@ public class ReportDaoImpl implements ReportDao {
 	@Override
 	public int insertReport(SqlSessionTemplate session, Report r) {
 		return session.insert("report.insertReport",r);
+	}
+
+	@Override
+	public List<Report> selectReport(SqlSessionTemplate session,int cPage, int numPerPage) {
+		return session.selectList("report.selectReport",null, new RowBounds((cPage-1)*numPerPage, numPerPage));
+	}
+
+	@Override
+	public int selectReportCount(SqlSessionTemplate session) {
+		return session.selectOne("report.selectReportCount");
 	}
 
 }
